93613c1cc70ad2a540f9bb5f10ff77f22992b488
Asko Nõmm <asko@digitalpress.co>
2017-01-15 13:28:32 +0200

    bump

--- includes/connect.php
+++ includes/connect.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once __DIR__ . '/../../idiorm.php';
+
 ORM::configure('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME);
 ORM::configure('username', DB_USER);
 ORM::configure('password', DB_PASSWORD);
